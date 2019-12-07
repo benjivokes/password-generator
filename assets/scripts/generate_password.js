03f55a5c3510050d03e71efe238ae33ab48d1da9
Benjamin Vokes <ben.mym.ac>
2019-12-07 10:16:28 -0800

    Wrote function to copy to clipboard

--- assets/scripts/generate_password.js
+++ assets/scripts/generate_password.js
@@ -1,9 +1,18 @@
 //Generate Password script
+var generatedPassword = null;
 
 function generatePassword() {
 	alert("Password Generated");
 }
 
 function copyPassword() {
-	alert("Password copied");
+	//Copy the generated password to the clipboard
+	if (generatedPassword == null) {
+		alert("Nothing copied because a password has not been generated.")
+	} else {
+		let password = document.getElementById('generatedPassword');
+		password.select();
+		document.execCommand('copy');
+		alert("Password copied");
+	}
 }

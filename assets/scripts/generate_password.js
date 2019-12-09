9d396269b6a7524445df0e9cfc0765845409475e
Benjamin Vokes <ben.mym.ac>
2019-12-08 18:06:55 -0800

    Worked to validateInput and converted indents. VI now has the input values places into object. Object will be returned if validation is successful. If validation is not successful, the function will return as false. In generatePassword the input is assigned to the output of VI and an alert will appear if false is received.

--- assets/scripts/generate_password.js
+++ assets/scripts/generate_password.js
@@ -11,53 +11,71 @@ function getRandomInteger(limit) {
 }
 
 function getPossibleCharacters(){
-	let sourceCharsString = "";
-	sourceCharsString = sourceCharsString.concat(numeric,upperCase,lowerCase,specialChars);
-	return sourceCharsString.split("");
+    let sourceCharsString = "";
+    sourceCharsString = sourceCharsString.concat(numeric,upperCase,lowerCase,specialChars);
+    return sourceCharsString.split("");
 }
 
 function validateInput(){
-	//code to validate the inputs
-	const includeNumeric = document.getElementById('includeNumeric').checked;
-	const includeLower = document.getElementById('includeLower').checked;
-	const includeUpper = document.getElementById('includeUpper').checked;
-	const includeSpecial = document.getElementById('includeSpecial').checked;
-	const passwordLength = parseInt(document.getElementById('passwordLength').value,10);
-	let alertString = "Numeric: " + includeNumeric + "\nLower: " + includeLower + "\nUpper: " + includeUpper + "\nSpecial: " + includeSpecial + "\nLength: " + passwordLength;
-	window.alert(alertString);
-	//Code to verify at least one checkbox is checked
-	if ((includeNumeric == false) && (includeSpecial == false) && (includeLower == false) && (includeUpper ==false)) {
-		window.alert("There are no characters to use in the password! Tick at least one box.");
-	}
-	if ((passwordLength < 8) || (passwordLength > 128)) {
-		window.alert("The length you specified is not between 8 and 128!")
-	}
+    //code to validate the inputs
+    //returns false if input not validated
+    //otherwise returns object of input values
+    var validatedInput = {
+        includeNumeric: document.getElementById('includeNumeric').checked,
+        includeLower: document.getElementById('includeLower').checked,
+        includeUpper: document.getElementById('includeUpper').checked,
+        includeSpecial: document.getElementById('includeSpecial').checked,
+        passwordLength: parseInt(document.getElementById('passwordLength').value,10)
+    }
+    let alertString = "Numeric: " + validatedInput.includeNumeric +
+        "\nLower: " + validatedInput.includeLower + 
+        "\nUpper: " + validatedInput.includeUpper + 
+        "\nSpecial: " + validatedInput.includeSpecial + 
+        "\nLength: " + validatedInput.passwordLength;
+    window.alert(alertString);
+    //Code to verify at least one checkbox is checked
+    if ((validatedInput.includeNumeric == false) &&
+        (validatedInput.includeSpecial == false) &&
+        (validatedInput.includeLower == false) &&
+        (validatedInput.includeUpper ==false)) {
+        window.alert("There are no characters to use in the password! Tick at least one box.");
+        return false;
+    }
+    if ((validatedInput.passwordLength < 8) || (validatedInput.passwordLength > 128)) {
+        window.alert("The length you specified is not between 8 and 128!")
+        return false;
+    }
+    return validatedInput;
 }
 
+
 function generatePassword() {
-	validateInput();
-	let sourceCharsArray = getPossibleCharacters();
-	let newPassword = "";
-	for (i=0; i < passwordLength; i++) {
-		let randNum = getRandomInteger(sourceCharsArray.length);
-		let associatedChar = sourceCharsArray[randNum];
-		newPassword = newPassword.concat(associatedChar);
-	}
-	generatedPassword = newPassword;
-	//set the field with the password
-	document.getElementById('generatedPassword').value = newPassword;
-	document.getElementById('generatedPassword').style.display = "block";
-	document.getElementById('copyPassword').style.display = "inline";
+    let validatedInput = validateInput();
+    if (!validatedInput) {
+        window.alert("Not valid input.");
+    }
+    let sourceCharsArray = getPossibleCharacters();
+    let newPassword = "";
+    for (i=0; i < passwordLength; i++) {
+        let randNum = getRandomInteger(sourceCharsArray.length);
+        let associatedChar = sourceCharsArray[randNum];
+        newPassword = newPassword.concat(associatedChar);
+    }
+    generatedPassword = newPassword;
+    //set the field with the password
+    document.getElementById('generatedPassword').value = newPassword;
+    document.getElementById('generatedPassword').style.display = "block";
+    document.getElementById('copyPassword').style.display = "inline";
 }
 
 function copyPassword() {
-	//Copy the generated password to the clipboard
-	if (generatedPassword == null) {
-		alert("Nothing copied because a password has not been generated.")
-	} else {
-		let password = document.getElementById('generatedPassword');
-		password.select();
-		document.execCommand('copy');
-		alert("Password copied");
-	}
+    //Copy the generated password to the clipboard
+    if (generatedPassword == null) {
+        alert("Nothing copied because a password has not been generated.")
+    } else {
+        let password = document.getElementById('generatedPassword');
+        password.select();
+        document.execCommand('copy');
+        alert("Password copied");
+    }
 }
